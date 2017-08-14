f29e95cb39cafe0e9a1906bb9600d4be3d453162
niconiconi <niconicoflag@gmail.com>
2017-08-14 16:05:38 +0800

    加上GUID

--- MyFilm/CommonString.cs
+++ MyFilm/CommonString.cs
@@ -15,11 +15,16 @@ namespace MyFilm
 
         public static String WebSearchKeyWord = String.Empty;
 
-        public readonly static String PipeName = "myfilm_pipe";
-        public readonly static String MemoryMappedName = "myfilm_memory_mapped";
+        public readonly static String PipeName =
+            "myfilm_pipe_{8AC5703C-D6F4-43F0-B625-11D27D2ADCF8}";
+        public readonly static String MemoryMappedName =
+            "myfilm_memory_mapped_{98516080-4D5E-4A73-AA2D-37CC7AACCA07}";
 
-        public readonly static String SharedMemorySemaphoreReadName = "myfilm_SM_SR";
-        public readonly static String SharedMemorySemaphoreWriteName = "myfilm_SM_SW";
-        public readonly static String AppMutexName = "myfilm_app_mutex";
+        public readonly static String SharedMemorySemaphoreReadName =
+            "myfilm_SM_SR_{203FD686-411F-40A4-ACC3-DE3528E1EC32}";
+        public readonly static String SharedMemorySemaphoreWriteName =
+            "myfilm_SM_SW_{C039F665-E406-44D1-998D-73C9DA19CECE}";
+        public readonly static String AppMutexName =
+            "myfilm_app_mutex_{2D9D20B5-555B-49BA-A0C2-1CDCB2A255F7}";
     }
 }
